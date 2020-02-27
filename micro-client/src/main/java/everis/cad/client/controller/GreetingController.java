ebaa6f7d69481065ae2fa9f6f0ad58d46f6832c6
Raul Gogna <rkgogna27@gmail.com>
2020-02-27 16:49:44 +0100

    Delete GreetingController

--- micro-client/src/main/java/everis/cad/client/controller/GreetingController.java
+++
@@ -1,8 +0,0 @@
-package everis.cad.client.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-
-public interface GreetingController {
-    @RequestMapping("/greeting")
-    String greeting();
-}
